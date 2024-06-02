4669a25bcbb150851aba0366cbb41ad6f78af5ac
engmhmdsamy <93443397+engmhmdsamy@users.noreply.github.com>
2024-06-03 01:47:53 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,194 +1,28 @@
-var siteName = document.getElementById("bookmarkName");
-var siteURL = document.getElementById("bookmarkURL");
-var submitBtn = document.getElementById("submitBtn");
-var tableContent = document.getElementById("tableContent");
-var deleteBtns;
-var visitBtns;
-var closeBtn = document.getElementById("closeBtn");
-var boxModal = document.querySelector(".box-info");
-var bookmarks = [];
+//Use an array to hold the value of the quotes
+const QuoteText = [
+  {'author': 'Jim Rohn', 
+   'quote': 'Beware of what you become in pursuit of what you want.'
+  },
+  {'author': 'Epictetus', 
+   'quote': 'It\'s not what happens to you, but how you react to it that matters.'
+  },
+  {'author': 'Frank Sinatra', 
+   'quote': 'The best revenge is massive success.'
+  },
+  {'author': 'Wayne Gretzy', 
+   'quote': 'You miss 100% of the shots you don\'t take.'
+  },
+  {'author': 'Nelson Mandela', 
+   'quote': 'Resentment is like drinking poison and waiting for your enemies to die.'
+  },
+  {'author': 'Elbert Hubbard', 
+   'quote': 'Do not take life too seriously. You will not get out alive.'
+  },
+];
 
-
-
-
-
-if (localStorage.getItem("bookmarksList")) {
-  bookmarks = JSON.parse(localStorage.getItem("bookmarksList"));
-  for (var x = 0; x < bookmarks.length; x++) {
-    displayBookmark(x);
-  }
+function generateQuote(){
+  const random = Number.parseInt(Math.random()*QuoteText.length + 1);
+  document.querySelector('#quoteOutput').textContent = `\"${QuoteText[random].quote}\"`;
+  document.querySelector('#authorOutput').textContent = `--${QuoteText[random].author}`;
+  
 }
-
-
-
-
-
-
-function displayBookmark(indexOfWebsite) {
-  var userURL = bookmarks[indexOfWebsite].siteURL;
-  var httpsRegex = /^https?:\/\//g;
-  if (httpsRegex.test(userURL)) {
-    validURL = userURL;
-    fixedURL = validURL
-      .split("")
-      .splice(validURL.match(httpsRegex)[0].length)
-      .join("");
-  } else {
-    var fixedURL = userURL;
-    validURL = `https://${userURL}`;
-  }
-
-
-        var newBookmark = `
-              <tr>
-                <td>${indexOfWebsite + 1}</td>
-                <td>${bookmarks[indexOfWebsite].siteName}</td>              
-                <td>
-                  <button class="btn btn-visit" data-index="${indexOfWebsite}">
-                    <i class="fa-solid fa-eye pe-2"></i>Visit
-                  </button>
-                </td>
-                <td>
-                  <button class="btn btn-delete pe-2" data-index="${indexOfWebsite}">
-                    <i class="fa-solid fa-trash-can"></i>
-                    Delete
-                  </button>
-                </td>
-            </tr>
-            `;
-  tableContent.innerHTML += newBookmark;
-  deleteBtns = document.querySelectorAll(".btn-delete");
-  if (deleteBtns) {
-    for (var j = 0; j < deleteBtns.length; j++) {
-      deleteBtns[j].addEventListener("click", function (e) {
-        deleteBookmark(e);
-      });
-    }
-  }
-
-
-
-  visitBtns = document.querySelectorAll(".btn-visit");
-  if (visitBtns) {
-    for (var l = 0; l < visitBtns.length; l++) {
-      visitBtns[l].addEventListener("click", function (e) {
-        visitWebsite(e);
-      });
-    }
-  }
-}
-
-
-
-function clearInput() {
-  siteName.value = "";
-  siteURL.value = "";
-}
-
-
-
-
-function capitalize(str) {
-  let strArr = str.split("");
-  strArr[0] = strArr[0].toUpperCase();
-  return strArr.join("");
-}
-
-
-submitBtn.addEventListener("click", function () {
-  if (
-    siteName.classList.contains("is-valid") &&
-    siteURL.classList.contains("is-valid")
-  ) {
-    var bookmark = {
-      siteName: capitalize(siteName.value),
-      siteURL: siteURL.value,
-    };
-    bookmarks.push(bookmark);
-    localStorage.setItem("bookmarksList", JSON.stringify(bookmarks));
-    displayBookmark(bookmarks.length - 1);
-    clearInput();
-    siteName.classList.remove("is-valid");
-    siteURL.classList.remove("is-valid");
-  } else {
-    boxModal.classList.remove("d-none");
-  }
-});
-
-
-
-
-function deleteBookmark(e) {
-  tableContent.innerHTML = "";
-  var deletedIndex = e.target.dataset.index;
-  bookmarks.splice(deletedIndex, 1);
-  for (var k = 0; k < bookmarks.length; k++) {
-    displayBookmark(k);
-  }
-  localStorage.setItem("bookmarksList", JSON.stringify(bookmarks));
-}
-
-
-
-function visitWebsite(e) {
-  var websiteIndex = e.target.dataset.index;
-  var httpsRegex = /^https?:\/\//;
-  if (httpsRegex.test(bookmarks[websiteIndex].siteURL)) {
-    open(bookmarks[websiteIndex].siteURL);
-  } else {
-    open(`https://${bookmarks[websiteIndex].siteURL}`);
-  }
-}
-
-
-
-
-
-
-var nameRegex = /^\w{3,}(\s+\w+)*$/;
-var urlRegex = /^(https?:\/\/)?(w{3}\.)?\w+\.\w{2,}\/?(:\d{2,5})?(\/\w+)*$/;
-
-siteName.addEventListener("input", function () {
-  validate(siteName, nameRegex);
-});
-
-siteURL.addEventListener("input", function () {
-  validate(siteURL, urlRegex);
-});
-
-function validate(element, regex) {
-  var testRegex = regex;
-  if (testRegex.test(element.value)) {
-    element.classList.add("is-valid");
-    element.classList.remove("is-invalid");
-  } else {
-    element.classList.add("is-invalid");
-    element.classList.remove("is-valid");
-  }
-}
-
-
-function closeModal() {
-  boxModal.classList.add("d-none");
-}
-
-
-
-
-closeBtn.addEventListener("click", closeModal);
-
-
-
-document.addEventListener("keydown", function (e) {
-  if (e.key == "Escape") {
-    closeModal();
-  }
-});
-
-
-
-document.addEventListener("click", function (e) {
-  if (e.target.classList.contains("box-info")) {
-    closeModal();
-  }
-});
